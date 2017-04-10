869216ae3014c7b2af144e0c974cc932dabe496e
Jeroen Staab <jstaab@posteo.de>
2017-04-11 01:52:47 +0200

    Testing tweet.R sucessfull. Docu impro

--- extra/tweet.R
+++ extra/tweet.R
@@ -1,21 +1,20 @@
 #' Make PaspberryPi post status to twitter
 #' This twitterbot allows to actually stay in touch with the instrument.
-#' Run this script at reboot, to be informed about IP adress
 #' Opt-In crontab
 
+## Dependencie
+library(twitteR)
 
+## Input
+#' msg
+#' mediaPath
 
-# Init API
+## Init API
 keyfile <- "./extra/twitter_credentials.R"
 source(keyfile)
-
-library(twitteR)
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-
-
-# Compose Tweet
-
-# Tweet 🚀
-msg <- paste(tweet, collapse = "\n")  
-tweet(msg)
+## Compose Tweet
+msg <- paste(msg, collapse = "\n")  
+## Tweet 🚀
+tweet(msg, mediaPath = mediaPath)
