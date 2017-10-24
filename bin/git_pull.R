571cb2447d5dca37ee1092fe9eaea057d65d970d
Jeroen Staab <jstaab@posteo.de>
2017-10-24 21:53:00 +0200

    Gibt `dev` parameter mehr gewicht

--- bin/git_pull.R
+++ bin/git_pull.R
@@ -1,7 +1,7 @@
 ## git pull Logfile
 system("git pull")
 Logfile.latest <- read.csv("Logfile.csv", stringsAsFactors = FALSE)
-if(sendung %in% Logfile.latest[which(as.character(date) == Logfile.latest[[1]]), 2])
+if(!dev && sendung %in% Logfile.latest[which(as.character(date) == Logfile.latest[[1]]), 2])
     stop("Diese Sendung wurde schon prozessiert.")
 
 
