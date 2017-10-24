17072d95ba2c124a5253303de7f55c3764aa55ac
mwip <mwip@users.noreply.github.com>
2017-10-24 22:28:57 +0200

    added heuteStatistik.png to git commit

--- bin/git_push.R
+++ bin/git_push.R
@@ -1,5 +1,6 @@
 ## git push Logfile
 commit <- paste(sendung, date)
 system("git add Logfile.csv")
+system("git add heuteStatistik.png")
 system(paste('git commit -m "', commit, '"'))
 system("git push")
