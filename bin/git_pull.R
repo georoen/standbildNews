c33bfa22425049907777b5a1ca0f807ceab94273
mwip <mwip@noreply.github.com>
2017-10-28 14:00:59 +0200

    Klammern in hinzugefügt

--- bin/git_pull.R
+++ bin/git_pull.R
@@ -1,8 +1,9 @@
 ## git pull Logfile
 system("git pull")
 Logfile.latest <- read.csv("Logfile.csv", stringsAsFactors = FALSE)
-if(!dev && sendung %in% Logfile.latest[which(as.character(date) == Logfile.latest[[1]]), 2])
-    stop("Diese Sendung wurde schon prozessiert.")
+if(!dev && sendung %in% Logfile.latest[which(as.character(date) == Logfile.latest[[1]]), 2]){
+  stop("Diese Sendung wurde schon prozessiert.")
+}
 
 
 
