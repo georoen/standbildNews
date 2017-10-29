dea454d9c2c55293528c0ed637597ec20b86eb46
mwip <mwip@noreply.github.com>
2017-10-29 13:32:56 +0100

    git add in git stage geändert.

--- bin/git_push.R
+++ bin/git_push.R
@@ -1,6 +1,6 @@
 ## git push Logfile
 commit <- paste(sendung, date)
-system("git add Logfile.csv")
-system("git add heuteStatistik.png")
+system("git stage Logfile.csv")
+system("git stage heuteStatistik.png")
 system(paste('git commit -m "', commit, '"'))
 system("git push")
