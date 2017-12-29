af3f8a6bed4c79d0b7de378caf2901d4bd519701
Jeroen Staab <jstaab@posteo.de>
2017-12-30 00:06:00 +0100

    Include `absolutaDauer` and add `absolutZensiert` to tweeted `msg` (Tipp Salih). + Small ggplot details.

--- bin/tweet.R
+++ bin/tweet.R
@@ -16,15 +16,15 @@ setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 ## Modify Tweet
 # #Hashtags
-msg[1] <- gsub("ARD", "#ARD", msg[1])
-msg[1] <- gsub("ZDF", "#ZDF", msg[1])
+msg <- gsub("ARD", "#ARD", msg)
+msg <- gsub("ZDF", "#ZDF", msg)
 # or @Mentions
 # msg[1] <- gsub("ARD", "ARD @tagesschau", msg[1])
 # msg[1] <- gsub("ZDF HeuteJournal", "@heutejournal", msg[1])  # 1.
 # msg[1] <- gsub("ZDF Heute", "@ZDFheute", msg[1])  # 2. Reihenfolge wichtig!
 
 ## Compose Tweet
-(msg <- paste(msg, collapse = "\n"))
+# (msg <- paste(msg, collapse = "\n"))
 
 ## Tweet 🚀
 if(!dev)
