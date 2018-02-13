194489ba9a47bf12cb1c2839ce9cbf0f9420c736
Jeroen Staab <jstaab@posteo.de>
2018-02-13 22:06:57 +0100

    Filling Datagaps. Unfortunatly still strugelling with converting `date` back, thus Bug with Edges in incomplete weeks (e.g. tth 48)

--- app_heatmap.R
+++ app_heatmap.R
@@ -5,47 +5,59 @@ library(lubridate)
 logdata <- read_csv("https://georoen.github.io/heuteZensiert/Logfile.csv") %>% 
   select(date:prozent) %>% 
   transmute(date = as.Date(date),
-            sender = factor(ifelse(grepl("h", sendung), "ZDF", "ARD")),
+            sender = factor(ifelse(grepl("^h", sendung), "ZDF", "ARD")),
             sendung = factor(sendung, labels = c("Heute 19Uhr", "Heute Journal",
                                                  "Tageschau", "Tagesthemen")),
             prozent = as.numeric(gsub("%", "", prozent))) %>% 
+  arrange(desc(date))
+
+# Preprocess
+df <- logdata %>% 
   mutate(WDay = lubridate::wday(date, label = T, abbr = F, week_start = 1),
          WDay = forcats::fct_rev(WDay),
          Week = lubridate::isoweek(date),  # Isoweek starts with Monday
          Month = lubridate::month(date),
          Year = lubridate::year(date)) %>% 
-  arrange(desc(date))
-
-edges <- logdata %>% 
+  arrange(desc(date)) %>% 
   # Fill Missing Values (Weekly)
-  # spread(WDay, prozent) %>%
-  # gather(WDay, prozent, 6:13) %>%
-  #
+  group_by(sender, sendung, Year) %>%
+  nest() %>% 
+  mutate(data = map(data, function(x){x %>% complete(Week, WDay)})) %>% 
+  unnest() %>% 
+  mutate(date = if_else(!is.na(date), date, # Fill missing Dates
+                        as.Date(paste(WDay, Week, Year), "%A %V %Y")),
+         Month = lubridate::month(date))
+
+# Month Grid
+edges <- df %>% 
+  # As numeric to ease shifting
+  mutate(WDay = as.numeric(WDay),
+         Week = as.numeric(Week)) %>% 
+  # Select frist 7 days of month.
   filter(lubridate::day(date) <= 7) %>%
-  # Close vertical line under "Sunday" and Bottom hline
-  group_by(Year, Month) %>% 
+  # Add day 0. Closes vertical line under "Sunday" and Bottom hline
+  group_by(sendung, Year, Month) %>% 
   select(WDay, Week) %>% 
-  distinct() %>% 
   nest() %>% 
   mutate(data = map(data, function(x){
     x %>% 
       rbind(c(0, min(x$Week))) %>%  # Add Point at Day = 0.5 in first weerk
       arrange(WDay)})) %>% 
   unnest() %>% 
-  # Style 4 geom_tile
-  mutate(Week = as.numeric(Week)-0.5,
-         WDay = as.numeric(WDay)+0.5) 
+  # Shift by 0.5 to match geom_tile()
+  mutate(Week = Week-0.5,
+         WDay = WDay+0.5) 
   
 
 # Plot
-ggplot(logdata, aes(Week, as.numeric(WDay))) +
+ggplot(df, aes(Week, as.numeric(WDay))) +
   geom_tile(aes(fill = prozent)) +
+  scale_fill_continuous(low = "lightgreen", high = "red") +
+  # Month Grid
   # geom_point(data = edges) +
   geom_step(data = edges, aes(group=Month)) +
   geom_hline(yintercept = c(0.5, 7.5)) +
-  # geom_contour(aes(x = Week, y = as.numeric(Day), z = Month), binwidth = 1) +
-  # geom_contour(aes(z = Month), binwidth = 1) +
-  scale_fill_continuous(low = "lightgreen", high = "red") +
-  scale_y_continuous(breaks = c(1:7), labels = levels(logdata$WDay)) +
+  # Scales, Labs, Theme
+  scale_y_continuous(breaks = c(1:7), labels = levels(df$WDay)) +
   facet_grid(sendung ~ Year, scales = "free_x") +
   labs(x = "Kalenderwoche", y = "") 
