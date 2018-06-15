georoen/standbildNews

commit e32231d37a1c796b4768980014d40382b4919c13

docu improvement + skipping frame download in dev mode

bin/heuteZensiert.R
@@ -106,33 +106,25 @@ source2 <- function(file, ...) {
 # args <- list(sen = "h19", date = format(Sys.Date(), "%Y%m%d"))
 args <- commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
-  ### Keine Argumente. Run Defaults = 19Uhr von date( <HEUTE> )
+  ### Keine Argumente. Default ist h19 von heute.
   warning("Keine Argumente. Verwende default", call.=FALSE)
   # sendung <- "t20"
   sendung <- "h19"
-  date <- Sys.Date()
-  dateshift <- 0
-  
-  # Wenn es heute noch vor 19 Uhr ist, wird der gestrige Tag angenommen, da 
-  # heutiges Video noch nicht online. 
-  if (lubridate::hour(Sys.time()) < 19){
-    date <- date - 1
-  }
+  Sys.Date() - (dateshift <- 0) # Heute
   
 } else if (length(args)==1) {
   ### Sendung angegeben. Datum fehlt
   sendung <- args[1]
-  date <- Sys.Date()  # Heute
-  dateshift <- 0
+  Sys.Date() - (dateshift <- 0) # Heute
   
   # Wenn es heute noch vor 19 (20, 23) Uhr ist, wird der gestrige Tag angenommen, da 
   # heutiges Video noch nicht online.
-  zeitDerSendung <- switch(sendung, 
+  zeitDerAustrahlung <- switch(sendung, 
                            h19 = 19, 
                            t20 = 20, 
                            hjo = 23, 
                            tth = 22)
-  if (lubridate::hour(Sys.time()) < zeitDerSendung){
+  if (lubridate::hour(Sys.time()) < zeitDerAustrahlung){
     date <- date - 1
   }
 
@@ -140,7 +132,7 @@ if (length(args)==0) {
   ### Sendung und Datum angegenen
   sendung <- args[1]
   dateshift <- as.numeric(unlist(args[2]))
-  date <- Sys.Date()-dateshift
+  date <- Sys.Date() - dateshift
   if(is.na(date)){
     stop("Argument 2 ist keine Zahl und kann nicht vom Datum abgezogen werden.")
   }
@@ -163,7 +155,7 @@ if(!dev){
 #### Download ####
 source2("download.R", chdir = TRUE)
 
-#### Texterkennung ####
+#### Frames Prozessieren ####
 # source2("mth_Classic.R")
 # source2("mth_OCR.R", chdir = TRUE)
 source2("mth_imageAlgebra.R", chdir = TRUE)

bin/download.R
@@ -1,19 +1,7 @@
 #' Download Stream
 
 
-# Nachrichtensendung herunterladen
-## Stream in Tempdir speichern
-Temp <- ifelse(!dev,  # dev = TRUE um Frames zu archivieren
-       paste0(tempdir(), "/", format(date, "%y%m%d"), "_", sendung, "/"),  # Tmp
-       paste0(wd, "/archiv/", format(date, "%y%m%d"), sendung, "/"))  # Archive Mode
-
-if(dir.exists(Temp))
-  stop("Directory exists. Avoid Dublicates")
-dir.create(Temp)
-TempImg <- paste0(Temp, "img%03d.jpg")
-
-
-## Paste0 URL
+#### URL Generieren ####
 # ARD
 #' tagesschau 20Uhr
 #' 23.4.17: http://download.media.tagesschau.de/video/2017/0423/TV-20170423-2033-4601.h264.mp4
@@ -117,6 +105,7 @@ compose_URL <- function(date, sendung, mode) {
     # ARD
     URL <- (compose_URL.ard(date, sendung))
   }
+  print(URL)  # Print URL to console - verbosity
   
   # Test it
   if(!is.null(URL) && !httr::http_error(URL)) {
@@ -126,24 +115,37 @@ compose_URL <- function(date, sendung, mode) {
   ifelse(dev, NA, stop ("Kann die gesuchte Sendung zu diesem Datum nicht finden!"))
 }
 
-
-
-## Download. Dauert ein paar Minuten...
 URL <- compose_URL(date, sendung)  # 28.10.2017
-(cmd <- paste("ffmpeg -i", URL, "-vf", paste0("fps=1/",res), TempImg))
-nokay <- try(system(cmd))
 
+#### Temp Directory für Frames ####
+## Stream in Tempdir speichern
+Temp <- ifelse(!dev,  # dev = TRUE um Frames zu archivieren
+               paste0(tempdir(), "/", format(date, "%y%m%d"), "_", sendung, "/"),  # Tmp
+               paste0(wd, "/archiv/", format(date, "%y%m%d"), sendung, "/"))  # Archive Mode
 
-if(nokay){
-  # Download hat immer noch nicht geklappt... Breche ab!
-  msg <- c(header(sendung, date))
-  (msg <- c(msg, "konnte nicht geladen werden."))
-  #unlink(Temp, recursive = TRUE)
-  # Twittern
-  mediaPath <- NULL
-  #source("extra/tweet.R")
-  # Stop
-  stop(paste("Streamfehler in", URL))
-}
+if(dir.exists(Temp)){
+  message("Directory exists. Skipping Download")
 
+} else {
+  ## New Folder
+  dir.create(Temp)
+  TempImg <- paste0(Temp, "img%03d.jpg")
+  
+  #### Download ####
+  (cmd <- paste("ffmpeg -i", URL, "-vf", paste0("fps=1/",res), TempImg))
+  nokay <- try(system(cmd))
+  
+  
+  if(nokay){
+    # Download hat immer noch nicht geklappt... Breche ab!
+    msg <- c(header(sendung, date))
+    (msg <- c(msg, "konnte nicht geladen werden."))
+    #unlink(Temp, recursive = TRUE)
+    # Twittern
+    mediaPath <- NULL
+    #source("extra/tweet.R")
+    # Stop
+    stop(paste("Streamfehler in", URL))
+  }
+}   
 

commit 831f46327e4ef50e505d6c3d5ea1d9d2a191fb1c

fixed bug introduced in e32231d3

bin/heuteZensiert.R
@@ -110,12 +110,12 @@ if (length(args)==0) {
   warning("Keine Argumente. Verwende default", call.=FALSE)
   # sendung <- "t20"
   sendung <- "h19"
-  Sys.Date() - (dateshift <- 0) # Heute
+  date <- Sys.Date() - (dateshift <- 0) # Heute
   
 } else if (length(args)==1) {
   ### Sendung angegeben. Datum fehlt
   sendung <- args[1]
-  Sys.Date() - (dateshift <- 0) # Heute
+  date <- Sys.Date() - (dateshift <- 0) # Heute
   
   # Wenn es heute noch vor 19 (20, 23) Uhr ist, wird der gestrige Tag angenommen, da 
   # heutiges Video noch nicht online.