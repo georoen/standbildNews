401e97c43e305302226b2f439f5cf340c17066b8
Jeroen Staab <jstaab@posteo.de>
2018-06-30 16:37:03 +0200

    second try...

--- bin/mediathekview.R
+++ bin/mediathekview.R
@@ -1,28 +1,38 @@
+# curl 'https://mediathekviewweb.de/api/query' -H 'content-type: text/plain' --data '{"queries":[{"fields":["title","topic"],"query":"sturm der liebe"},{"fields":["channel"],"query":"ndr"}],"sortBy":"timestamp","sortOrder":"desc","future":false,"offset":0,"size":10}'
+
+
+
+library(curl)
+h <- new_handle()
+handle_setopt(h, copypostfields = "moo=moomooo");
+handle_setheaders(h,
+                  "Content-Type" = "text/moo" #,
+                  # "Cache-Control" = "no-cache",
+                  # "User-Agent" = "A cow"
+)
+
+
+req <- curl_fetch_memory("http://httpbin.org/post", handle = h)
+cat(rawToChar(req$content))
+
+
+
+library(httr)
+# curl 'https://mediathekviewweb.de/api/query' -H 'content-type: text/plain' 
+--data '{"queries":[{"fields":["title","topic"],"query":"sturm der liebe"},{"fields":["channel"],"query":"ndr"}],
+"sortBy":"timestamp","sortOrder":"desc","future":false,"offset":0,"size":10}'
+res <- POST(url = "https://mediathekviewweb.de/api/query",
+            add_headers("Content-type" = "text/plain"),
+            # body = (list(queries = "{'channel':'ARD'}")),
+            # query = list(channel = "ARD"),
+            # query = list("!ard +tagesschau, 20:00 Uhr"),
+            verbose())
+
+
+
 library(rvest)
-
-newField <- function(field, query) {
-  paste0('{"fields":["', paste0(field, collapse = '","'), '","query":"', query, '"}')
-}
-
-newField(c("title", "topic"), "Tagesschau")
-newField("channel", "ARD")
-
-header <- "curl 'https://mediathekviewweb.de/api/query' -H 'content-type: text/plain'"
-data <- paste0("--data '{",
-               # Filters
-               paste0('"queries":[',
-                      newField(c("title", "topic"), "Tagesschau"),
-                      ",",
-                      newField("channel", "ARD"),
-                      "]"),
-               ",",
-               # Sorting
-               paste0('"sortBy":"timestamp","sortOrder":"desc","future":false,"offset":0,"size":10'),
-               "}'")
-
-(cmd <- paste(header, data))
-system(cmd)
-html(cmd)
-paste('curl 'https://mediathekviewweb.de/api/query' -H 'content-type: text/plain' 
-      --data '{"queries":[{"fields":["title","topic"],"query":"sturm der liebe"},{"fields":["channel"],"query":"ndr"}],
-               "sortBy":"timestamp","sortOrder":"desc","future":false,"offset":0,"size":10}'')
+url <- "https://mediathekviewweb.de/feed?query=!ard%20%2Btagesschau%2C%2020%3A00%20Uhr"
+rss <- read_html(url)
+str(rss)
+rss %>% 
+  html_node()
