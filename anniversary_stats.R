c9c6d5b21cbee71e9a31d9b1d9b826f1ab4c305b
Jeroen Staab <jstaab@posteo.de>
2018-12-04 19:32:24 +0100

    Adding another figure (colors don't work..)

--- anniversary_stats.R
+++ anniversary_stats.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(lubridate)
 library(ggplot2)
-library(ggjoy)
+library(ggridges)
 
 x <- read.csv("Logfile.csv", stringsAsFactors = FALSE)
 x$date <- as.Date(x$date)
@@ -29,11 +29,20 @@ x1sum <- x1 %>% group_by(sendung) %>% summarize(sender = first(sender),
                                                 censored_secs = sum(censored_secs))
 x1max <- x1 %>% group_by(sendung) %>% summarize(Prozent = max(prozent_num))
 
+# Plot 1: Prozent Treppchen
+x1mean <- x1 %>% group_by(sender, sendung) %>% summarize(Prozent = mean(prozent_num))
+ggplot(x1mean, aes(sendung, Prozent)) +
+  geom_col() +
+  geom_text(aes(label = round(Prozent,1)), nudge_y = 0.25, size = 20) +
+  scale_fill_manual(values = alpha(c("darkorange", "darkgrey", "#3284be", "dodgerblue4"), alpha = 0.7)) +
+  facet_wrap(~sender, scales = "free_x")
+
+# Plot 2: JOY heißt jetzt ggridges
 x1 %>% 
   ggplot(aes(y = sendung, x = prozent_num, group = sendung, fill = sendung)) + 
   # geom_boxplot(outlier.size = 0.5) + 
-  geom_violin() + 
-  geom_joy(color = "lightgrey") + 
+  # geom_violin() + 
+  geom_density_ridges2(color = "lightgrey", scale = 0.5) + 
   # geom_point(data = x1max, aes(x = sendung, y = Prozent, color = sendung), 
              # shape = 18, size = 5) + 
   # geom_label(data = x1max, aes(x = sendung, y = Prozent, label = paste("max.\n", Prozent, "%"))) + 
